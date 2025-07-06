607ce4f7ec96b55ddb89890b0a2890930057ecf6
jatink124 <kaushaljatin48@gmail.com>
2025-07-06 18:30:31 +0530

    added link origin

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:5173',
     'https://webdevgurus.online',
+    'https://mygrowthplanner.netlify.app/',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,
